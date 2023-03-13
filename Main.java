873c3aa189ab8e71b944edf92888539144c35d83
KongCPH <112859698+KongCPH@users.noreply.github.com>
2023-03-13 12:37:44 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,11 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        UserHandler userHandler = new UserHandler("users.txt");
-        TextUI textUI = new TextUI(userHandler);
+       public static void main(String[] args) {
+        // load fil med brugernavne/passwords
+        UserHandler userhandler = null;
+        try {
+            userhandler = new UserHandler("user.txt");
+            userhandler.loadUsers();
+        }catch(Exception e){
+            // skriv til bruger og luk program
+            System.out.println("Desværre kan vi ikke køre programmet lige nu");
+            System.exit(0);
+        }
 
-        userHandler.loadUsers();
-        
+         
         String userInput = textUI.getUserInput();
 
         switch (userInput) {
